f0c775f885f9236a017d529cd117eefc19753dac
dessykn <dessy_dkn@yahoo.com>
2015-09-21 15:40:40 +0700

    Update and rename list.h to sekolah.h

---
+++ LinkedList/sekolah.h
@@ -0,0 +1,21 @@
+#ifndef SEKOLAH_H_INCLUDED
+#define SEKOLAH_H_INCLUDED
+
+
+#include<iostream>
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+
+#define nil NULL
+#define info(p) (p)-> info
+#define next(p) (p)-> next
+#define first(l) ((l).first)
+
+using namespace std;
+
+void deallocate(address p);
+void insertlast(list *l, address p);
+void deletelast(list *l, address p);
+
+#endif // SEKOLAH_H_INCLUDED
