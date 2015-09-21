acec4c3a6085095d37819320d5cca9b62a930f43
Muhammad Azhari Rahman <azharirahmann@gmail.com>
2015-09-21 19:27:01 +0700

    Update sekolah.cpp

--- LinkedList/sekolah.cpp
+++ LinkedList/sekolah.cpp
@@ -1,38 +1,85 @@
-void createnewlist(list *l){
-    first(*l)=nil;
+address searchElement1(list *l, int a){
+    address p;
+    p=first(*l);
+    while (p!=nil){
+        if (info(p).no_induk==a){
+            return p;
+        }
+        p=next(p);
+    }
+    return nil;
 }
 
-void insertfirst(list *l, address p){
+void insertafter(list *l, address p, address prec){
+    address q;
     infotype x;
-    data_pelajar(&x);
-    p=allocate(x);
-    if (first(*l)==nil){
-        first(*l)=p;
-        next(first(*l))=nil;
-    }
-    else{
-        next(p)=first(*l);
-        first(*l)=p;
-    }
-}
-
-void deletefirst(list *l, address p){
+    int cari;
+    q=first(*l);
     if (first(*l)==nil){
         cout << "Data tidak ada \n";
-        cout << "Kembali ke Menu Utama. . .";
-    }
-    else if (next(first(*l))==nil){
-        first(*l)=nil;
-        deallocate(first(*l));
-        cout << "Delete Data Sukses ! \n";
-        cout << "Kembali ke Menu Utama. . .";
+        cout << "Kembali ke Menu Utama. . .\n";
     }
     else{
-        p=first(*l);
-        first(*l)=next(p);
-        next(p)=nil;
-        deallocate(p);
-        cout << "Delete Data Sukses ! \n";
-        cout << "Kembali ke Menu Utama. . .";
+        cout << "Input setelah data : ";
+        cin >> cari;
+        while (info(q).no_induk != cari){
+            q=next(q);
+        }
+        if (info(q).no_induk == nil){
+            cout << endl;
+            cout << "Nomor induk tidak ditemukan \n";
+            cout << "Kembali ke menu utama. . . \n";
+        }
+        else{
+            data_pelajar(&x);
+            p=allocate(x);
+            next(p)=next(q);
+            next(q)=p;
+            cout << endl;
+            cout << "Insert Data Sukses ! \n";
+            cout << "Kembali ke Menu Utama. . .";
+        }
+    }
+}
+
+void deleteafter(list *l, address *p, address prec){
+    int hps;
+    char pil;
+    if (first(*l)==nil){
+        cout << "Data tidak ada";
+    }
+    else if(next(first(*l))==nil){
+        cout << "Data yang ada hanya 1, apakah data ingin dihapus (Y/N) : ";
+        cin >> pil;
+        if (pil=='Y'){
+            first(*l)=nil;
+            deallocate(first(*l));
+            cout << "Delete Data Sukses ! \n";
+        }
+        else{
+            cout << "Kembali ke menu utama . . .";
+        }
+    }
+    else{
+        prec=first(*l);
+        cout << "Hapus setelah data : ";
+        cin >> hps;
+        while (info(prec).no_induk != hps){
+            prec=next(prec);
+        }
+        if (info(prec).no_induk == nil){
+            cout << endl;
+            cout << "Nomor induk tidak ditemukan \n";
+            cout << "Kembali ke menu utama. . . \n";
+        }
+        else{
+            *p=next(prec);
+            next(prec)=next(*p);
+            next(*p)=nil;
+            deallocate(*p);
+            cout << endl;
+            cout << "Delete Data Sukses ! \n";
+            cout << "Kembali ke Menu Utama. . .";
+        }
     }
 }
