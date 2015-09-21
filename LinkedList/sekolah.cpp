b60fec63fda067a37752991619e278204f55e712
Fauzan Rambang <fauzanrambang40@gmail.com>
2015-09-21 19:05:15 +0700

    Update sekolah.cpp

--- LinkedList/sekolah.cpp
+++ LinkedList/sekolah.cpp
@@ -1,40 +1,38 @@
-address allocate(infotype x){
-    address p=(address)malloc(sizeof(elmlist));
-    info(p)=x;
-    next(p)=nil;
-    return p;
+void createnewlist(list *l){
+    first(*l)=nil;
 }
 
-void data_pelajar(infotype *x){
-    infotype a;
-    cout << "Nomor induk : ";
-    cin >> a.no_induk;
-    cout << "Nama siswa : ";
-    cin >> a.nama;
-    cout << "Kelas : ";
-    cin >> a.kelas;
-    cout << "Tahun Lahir : ";
-    cin >> a.tahun_lahir;
-    cout << "Alamat : ";
-    cin >> a.alamat;
-    cout << endl;
-    *x=a;
-}
-
-void view (list *l){
-    address p;
+void insertfirst(list *l, address p){
+    infotype x;
+    data_pelajar(&x);
+    p=allocate(x);
     if (first(*l)==nil){
-        cout << "Data tidak ada";
+        first(*l)=p;
+        next(first(*l))=nil;
+    }
+    else{
+        next(p)=first(*l);
+        first(*l)=p;
+    }
+}
+
+void deletefirst(list *l, address p){
+    if (first(*l)==nil){
+        cout << "Data tidak ada \n";
+        cout << "Kembali ke Menu Utama. . .";
+    }
+    else if (next(first(*l))==nil){
+        first(*l)=nil;
+        deallocate(first(*l));
+        cout << "Delete Data Sukses ! \n";
+        cout << "Kembali ke Menu Utama. . .";
     }
     else{
         p=first(*l);
-        do{
-            cout << "Nomor induk : " << info(p).no_induk << endl;
-            cout << "Nama siswa  : " << info(p).nama << endl;
-            cout << "Kelas       : " << info(p).kelas << endl;
-            cout << "Tahun Lahir : " << info(p).tahun_lahir << endl;
-            cout << "Alamat      : " << info(p).alamat << endl << endl;
-            p=next(p);
-        }while(p!=nil);
+        first(*l)=next(p);
+        next(p)=nil;
+        deallocate(p);
+        cout << "Delete Data Sukses ! \n";
+        cout << "Kembali ke Menu Utama. . .";
     }
 }
